ef3138ae5e95a54ae651be28e989eeeb0ef6c60f
Sebastian Palacios <spalaci2@uncc.edu>
2020-05-01 20:14:52 +0000

    Image Upload

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
+  resources :posts
+  
   
   resources :articles do
     resources :comments
